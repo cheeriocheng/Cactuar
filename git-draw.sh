9e671747787798be5fecf303e72de2a793e2559d
cheng <gnuchex@gmail.com>
2016-04-24 21:31:49 -0700

    针千本

---
+++ git-draw.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+
+# Run this script inside a new empty git repo.
+
+echo "Created with Git Draw (http://github.com/ben174/git-draw)" > README.md
+git add README.md
+touch gf
+git add gf
+echo 0 > gf
+GIT_AUTHOR_DATE=2015-06-01T12:00:00 GIT_COMMITTER_DATE=2015-06-01T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+GIT_AUTHOR_DATE=2015-06-01T12:00:00 GIT_COMMITTER_DATE=2015-06-01T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-06-02T12:00:00 GIT_COMMITTER_DATE=2015-06-02T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 > gf
+GIT_AUTHOR_DATE=2015-06-02T12:00:00 GIT_COMMITTER_DATE=2015-06-02T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-06-04T12:00:00 GIT_COMMITTER_DATE=2015-06-04T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 > gf
+GIT_AUTHOR_DATE=2015-06-04T12:00:00 GIT_COMMITTER_DATE=2015-06-04T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-06-05T12:00:00 GIT_COMMITTER_DATE=2015-06-05T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+GIT_AUTHOR_DATE=2015-06-05T12:00:00 GIT_COMMITTER_DATE=2015-06-05T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-06-06T12:00:00 GIT_COMMITTER_DATE=2015-06-06T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 > gf
+GIT_AUTHOR_DATE=2015-06-06T12:00:00 GIT_COMMITTER_DATE=2015-06-06T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-06-09T12:00:00 GIT_COMMITTER_DATE=2015-06-09T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+GIT_AUTHOR_DATE=2015-06-09T12:00:00 GIT_COMMITTER_DATE=2015-06-09T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-06-11T12:00:00 GIT_COMMITTER_DATE=2015-06-11T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+GIT_AUTHOR_DATE=2015-06-11T12:00:00 GIT_COMMITTER_DATE=2015-06-11T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-06-14T12:00:00 GIT_COMMITTER_DATE=2015-06-14T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 > gf
+GIT_AUTHOR_DATE=2015-06-14T12:00:00 GIT_COMMITTER_DATE=2015-06-14T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-06-15T12:00:00 GIT_COMMITTER_DATE=2015-06-15T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+GIT_AUTHOR_DATE=2015-06-15T12:00:00 GIT_COMMITTER_DATE=2015-06-15T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-06-16T12:00:00 GIT_COMMITTER_DATE=2015-06-16T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 > gf
+GIT_AUTHOR_DATE=2015-06-16T12:00:00 GIT_COMMITTER_DATE=2015-06-16T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-06-17T12:00:00 GIT_COMMITTER_DATE=2015-06-17T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+GIT_AUTHOR_DATE=2015-06-17T12:00:00 GIT_COMMITTER_DATE=2015-06-17T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-06-18T12:00:00 GIT_COMMITTER_DATE=2015-06-18T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 > gf
+GIT_AUTHOR_DATE=2015-06-18T12:00:00 GIT_COMMITTER_DATE=2015-06-18T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-06-21T12:00:00 GIT_COMMITTER_DATE=2015-06-21T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+GIT_AUTHOR_DATE=2015-06-21T12:00:00 GIT_COMMITTER_DATE=2015-06-21T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-06-22T12:00:00 GIT_COMMITTER_DATE=2015-06-22T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-06-23T12:00:00 GIT_COMMITTER_DATE=2015-06-23T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-06-24T12:00:00 GIT_COMMITTER_DATE=2015-06-24T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-06-25T12:00:00 GIT_COMMITTER_DATE=2015-06-25T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+GIT_AUTHOR_DATE=2015-06-25T12:00:00 GIT_COMMITTER_DATE=2015-06-25T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-06-28T12:00:00 GIT_COMMITTER_DATE=2015-06-28T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 > gf
+GIT_AUTHOR_DATE=2015-06-28T12:00:00 GIT_COMMITTER_DATE=2015-06-28T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-06-29T12:00:00 GIT_COMMITTER_DATE=2015-06-29T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+GIT_AUTHOR_DATE=2015-06-29T12:00:00 GIT_COMMITTER_DATE=2015-06-29T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-06-30T12:00:00 GIT_COMMITTER_DATE=2015-06-30T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 > gf
+GIT_AUTHOR_DATE=2015-06-30T12:00:00 GIT_COMMITTER_DATE=2015-06-30T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-07-01T12:00:00 GIT_COMMITTER_DATE=2015-07-01T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+GIT_AUTHOR_DATE=2015-07-01T12:00:00 GIT_COMMITTER_DATE=2015-07-01T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-07-02T12:00:00 GIT_COMMITTER_DATE=2015-07-02T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 > gf
+GIT_AUTHOR_DATE=2015-07-02T12:00:00 GIT_COMMITTER_DATE=2015-07-02T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-07-03T12:00:00 GIT_COMMITTER_DATE=2015-07-03T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+GIT_AUTHOR_DATE=2015-07-03T12:00:00 GIT_COMMITTER_DATE=2015-07-03T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-07-04T12:00:00 GIT_COMMITTER_DATE=2015-07-04T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 > gf
+GIT_AUTHOR_DATE=2015-07-04T12:00:00 GIT_COMMITTER_DATE=2015-07-04T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-07-07T12:00:00 GIT_COMMITTER_DATE=2015-07-07T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+GIT_AUTHOR_DATE=2015-07-07T12:00:00 GIT_COMMITTER_DATE=2015-07-07T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-07-11T12:00:00 GIT_COMMITTER_DATE=2015-07-11T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+GIT_AUTHOR_DATE=2015-07-11T12:00:00 GIT_COMMITTER_DATE=2015-07-11T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-07-14T12:00:00 GIT_COMMITTER_DATE=2015-07-14T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 > gf
+GIT_AUTHOR_DATE=2015-07-14T12:00:00 GIT_COMMITTER_DATE=2015-07-14T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-07-15T12:00:00 GIT_COMMITTER_DATE=2015-07-15T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+GIT_AUTHOR_DATE=2015-07-15T12:00:00 GIT_COMMITTER_DATE=2015-07-15T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-07-16T12:00:00 GIT_COMMITTER_DATE=2015-07-16T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 > gf
+GIT_AUTHOR_DATE=2015-07-16T12:00:00 GIT_COMMITTER_DATE=2015-07-16T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-07-18T12:00:00 GIT_COMMITTER_DATE=2015-07-18T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 > gf
+GIT_AUTHOR_DATE=2015-07-18T12:00:00 GIT_COMMITTER_DATE=2015-07-18T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-1 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-08-01T12:00:00 GIT_COMMITTER_DATE=2015-08-01T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-08-08T12:00:00 GIT_COMMITTER_DATE=2015-08-08T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-08-14T12:00:00 GIT_COMMITTER_DATE=2015-08-14T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-08-22T12:00:00 GIT_COMMITTER_DATE=2015-08-22T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-09-05T12:00:00 GIT_COMMITTER_DATE=2015-09-05T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-09-12T12:00:00 GIT_COMMITTER_DATE=2015-09-12T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-09-17T12:00:00 GIT_COMMITTER_DATE=2015-09-17T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-09-19T12:00:00 GIT_COMMITTER_DATE=2015-09-19T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-09-25T12:00:00 GIT_COMMITTER_DATE=2015-09-25T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-10-03T12:00:00 GIT_COMMITTER_DATE=2015-10-03T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-10-17T12:00:00 GIT_COMMITTER_DATE=2015-10-17T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-10-24T12:00:00 GIT_COMMITTER_DATE=2015-10-24T12:00:00 git commit -a -m "gf" > /dev/null
+echo 1-0 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-10-31T12:00:00 GIT_COMMITTER_DATE=2015-10-31T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-11-04T12:00:00 GIT_COMMITTER_DATE=2015-11-04T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-11-06T12:00:00 GIT_COMMITTER_DATE=2015-11-06T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-11-12T12:00:00 GIT_COMMITTER_DATE=2015-11-12T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-11-20T12:00:00 GIT_COMMITTER_DATE=2015-11-20T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."GIT_AUTHOR_DATE=2015-11-28T12:00:00 GIT_COMMITTER_DATE=2015-11-28T12:00:00 git commit -a -m "gf" > /dev/null
+echo 0-0 > gf
+echo "Your commit history has been created!, now run 'git push' to push the repo to GitHub."
